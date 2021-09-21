509b8dc5a723e9738a202fb35368595f944e5023
Sina Marefat <Sinamatefat@gmail.com>
2021-09-21 22:24:29 +0430

    init

---
+++ src/main/java/com/websocket/test/websocketdemo/config/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package com.websocket.test.websocketdemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.math.BigDecimal;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+        System.out.println(first.subtract(second));
+    }
+    BigDecimal first = BigDecimal.valueOf(43.56000006);
+    BigDecimal second = BigDecimal.valueOf(43.42);
+
+
+}
---
+++ src/main/java/com/websocket/test/websocketdemo/resources/GreetingController.java
@@ -0,0 +1,25 @@
+package com.websocket.test.websocketdemo.resources;
+
+import com.websocket.test.websocketdemo.model.Greeting;
+import com.websocket.test.websocketdemo.model.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.HtmlUtils;
+
+@RestController
+public class GreetingController {
+
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+}
